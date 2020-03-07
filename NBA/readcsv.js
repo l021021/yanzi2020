ba6be2c8463ac240400e3a56b0b7cd189eecc9b5
l021021 <653498331@qq.com>
2020-03-07 13:18:02 +0800

    nba

--- NBA/readcsv.js
+++ NBA/readcsv.js
@@ -5,12 +5,11 @@ const csvFile = 'NBA\\NBA.csv'
 const resultString = FS.readFileSync(csvFile, { encoding: 'utf8' })
 
 let resultArray = []
-
-// const EloDto = {
-//     TeamAB: '', [
-//         GameID: '',
-//     ELOScore: '']
-// }
+var ELOObj = {
+    name: '',
+    round: 0,
+    ELO: 1000
+}
 
 resultArray = csvToObject(resultString)
 
@@ -33,8 +32,9 @@ function csvToObject(csvString) {
     return datas
 }
 
-const EloArray = []
+// const EloArray = []
 var teamSet = []
+// var ELOObj
 // teamSet.push0 = function (_stuff) {
 //     if (teamSet.indexof(_stuff) < 0) teamSet.push(_stuff)
 //     else console.log(_stuff)
@@ -46,17 +46,86 @@ var teamSet = []
 
 for (let index = 0; index < resultArray.length; index++) {
     // console.log(typeof (str))
-    if (teamSet.indexOf(resultArray[index].HomeTeam) < 0) teamSet.push(resultArray[index].HomeTeam)
+    if ((teamSet.indexOf(resultArray[index].HomeTeam) < 0) && (resultArray[index].HomeTeam != null)) teamSet.push(resultArray[index].HomeTeam)
     // teamSet.push0(resultArray[index].HomeTeam)
 
     // EloArray.push(JSON.stringify(JSON.parse(data))) // resultArray[0].HomeTeam
 }
 
+// 赛程排序
+
+resultArray.sort(function (a, b) { if (a.GameID > b.GameID) { return 1 } else { return -1 } })
+
+// 建立新的ROUND
+for (let index = 0; index < resultArray.length; index++) {
+    const e = resultArray[index]
+    e.Round = index
+}
+
+var TeamElos = []
+function getELO(t, r) {
+    for (let i = 0; i < TeamElos.length; i++) {
+        if (TeamElos[i].name === t && TeamElos[i].round === r) return TeamElos[i].ELO
+    }
+}
+
+function getLatestELO(t) {
+    var _round = 0
+    var _index = 0
+    for (let i = 0; i < TeamElos.length; i++) {
+        if ((TeamElos[i].name === t) && (TeamElos[i].round > _round)) {
+            _round = TeamElos[i].round
+            _index = i
+        }
+    }
+    return TeamElos[_index].ELO
+}
+
+function setELO(t, r, e) {
+    ELOObj.name = t
+    ELOObj.round = r
+    ELOObj.ELO = e
+
+    TeamElos.push(JSON.parse(JSON.stringify(ELOObj)))
+}
+
+function printELO(t) {
+    for (let i = 0; i < TeamElos.length; i++) {
+        if (TeamElos[i].name === t) console.log(TeamElos[i].round + '   ' + TeamElos[i].ELO)
+    }
+}
+// const ELOObj = { name: '', round: 0, ELO: 1000 }
 console.log(JSON.stringify(teamSet))
 
-// for (let index = 0; index < resultArray.length; index++) {
-//     const ele = array[index]
-//     // search for last ELO
-//     EloArray.
+// Write initial Elo
+for (let i = 0; i < teamSet.length; i++) { // 为每队建立初始值
+    // console.log(teamSet[i])
+    setELO(teamSet[i], 0, 1000)
+}
+console.log('Initial ELO set')
 
-// }
+for (let index = 0; index < resultArray.length; index++) { // season 过滤赛季
+    const _hometeam = resultArray[index].HomeTeam
+    const _awayteam = resultArray[index].AwayTeam
+    const _homeWin = parseInt((resultArray[index].HOME == 'HOME') ? (resultArray[index].Result == 'W' ? 1 : 0) : (resultArray[index].Result == 'L' ? 1 : 0))
+    const _round = resultArray[index].Round
+    const _season = resultArray[index].Season
+
+    // console.log(_hometeam + '  ' + _round + '  ' + _season + '  ')
+    if (_season === '2016') {
+        var o1 = getLatestELO(_hometeam) || 1000
+        var o2 = getLatestELO(_awayteam) || 1000
+        // 计算等级分
+        const Ea = 1 / (1 + Math.pow(10, (o2 - o1) / 400))
+        const Eb = 1 / (1 + Math.pow(10, (o1 - o2) / 400))
+        const _newELO1 = parseFloat(o1) + 16 * (_homeWin - Ea)
+        const _newELO2 = parseFloat(o2) + 16 * ((1 - _homeWin) - Eb)
+
+        setELO(_hometeam, parseInt(_round), parseFloat(_newELO1))
+        setELO(_awayteam, parseInt(_round), parseFloat(_newELO2))
+    }
+}
+
+TeamElos.sort(function (a, b) { return a.round - b.round })
+// console.log(JSON.stringify(TeamElos))
+printELO('GSW')
